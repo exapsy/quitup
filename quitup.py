1fbb5959c7556443c490d4e21a997b94e1287af1
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-02-21 18:19:00 +0200

    Fix version to 3.8

--- quitup.py
+++ quitup.py
@@ -1,6 +1,6 @@
 import sys
-if sys.version_info[0] < 3:
-    raise Exception("Must be using Python 3")
+if sys.version_info[0] < 3 or sys.version_info[1] < 8:
+    raise Exception("Must be using Python 3.8")
 
 import cmd.checkPlatform
 import cmd.argParser
